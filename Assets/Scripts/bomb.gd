b5d6f60897e64423958d1a15582e728b27b5ff47
Ramiro Garro <ramagarro@gmail.com>
2024-06-12 21:54:50 -0300

    Entrega parcial v1

--- Assets/Scripts/bomb.gd
+++ Assets/Scripts/bomb.gd
@@ -7,6 +7,7 @@ func _on_body_entered(body):
 		$AreaExplosion/CollisionExplosion.disabled = false
 		$Sprites.set_animation("explosion")
 		$Sprites.play()
+		$Sound.play()
 		await $Sprites.animation_finished
 		$AreaExplosion/CollisionExplosion.disabled = true
 		self.queue_free()
