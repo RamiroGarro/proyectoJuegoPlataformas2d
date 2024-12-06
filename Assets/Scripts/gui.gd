5e662698d25fdf8e565e405fb88e685515c7156b
Ramiro Garro <ramagarro@gmail.com>
2024-12-06 16:59:44 -0300

    Creación de controles y exportación para Android

--- Assets/Scripts/gui.gd
+++ Assets/Scripts/gui.gd
@@ -4,17 +4,19 @@ func _process(delta):
 	$Container/Label.text = "SCORE: " + str(Global.score)
 
 func game_over() -> void:
-	#await get_tree().create_timer(1.5).timeout
 	get_tree().paused = true
-	$GameOver/Container/Buttons/Restart.grab_focus()
+	$Control.visible = false
+	$GameOver.show()
+	#$GameOver/Container/Buttons/Restart.grab_focus()
 	var tween : Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 	tween.tween_property($GameOver,'modulate',Color(1,1,1,0.8),1.0)
 	$GameOver/Sound.play()
 	
 func victory() -> void:
-	#await get_tree().create_timer(1.5).timeout
 	get_tree().paused = true
-	$Victory/Container/Buttons/Restart.grab_focus()
+	$Control.visible = false
+	$Victory.show()
+	#$Victory/Container/Buttons/Restart.grab_focus()
 	var tween : Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 	tween.tween_property($Victory,'modulate',Color(1,1,1,0.8),1.0)
 	$Victory/Sound.play()	
@@ -26,14 +28,19 @@ func _on_exit_pressed():
 	get_tree().quit()
 	 
 func _on_resume_pressed():
-	$Pause/Container/Buttons/Resume.release_focus()
+	#$Pause/Container/Buttons/Resume.release_focus()
 	get_tree().paused = false
+	$Control.visible = true
 	var tween : Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 	tween.tween_property($Pause,'modulate',Color(1,1,1,0),1.0)
+	await tween.finished
+	$Pause.hide()
 
 func _input(event):
 	if event.is_action_pressed("ui_pause") and get_tree().paused == false:
 		get_tree().paused = true
-		$Pause/Container/Buttons/Resume.grab_focus()
+		$Control.visible = false
+		$Pause.show()
+		#$Pause/Container/Buttons/Resume.grab_focus()
 		var tween : Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 		tween.tween_property($Pause,'modulate',Color(1,1,1,0.8),1.0)
--- Assets/Scripts/level.gd
+++ Assets/Scripts/level.gd
@@ -4,6 +4,9 @@ func _ready():
 	get_tree().paused = false
 	Global.score = 0
 	$Audio/Background.play()
+	$GUI/Pause.hide()
+	$GUI/Victory.hide()
+	$GUI/GameOver.hide()
 
 func _on_background_finished():
 	$Audio/Background.play()
